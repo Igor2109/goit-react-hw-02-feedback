1e7b00a263dec7987dbab2c28bf0b0c51b999e0c
Igor2109 <127054357+Igor2109@users.noreply.github.com>
2023-09-24 19:20:41 +0200

    components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import css from "./App.module.css"
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h2>Please leave feedback</h2>
+      <button type="button" className={css.btn}>
+        Good
+      </button>
+      <button type="button" className={css.btn}>
+        Neutral
+      </button>
+      <button type="button" className={css.btn}>
+        Bad
+      </button>
+
+      <h2>Statistics</h2>
+<li>
+  <ul
+  className="good" >Good:
+  </ul>
+  <ul
+  className="neutral" >Neutral:
+  </ul>
+  <ul
+  className="bad" >Bad:
+  </ul>
+  <ul
+  className="total" >Total:
+  </ul>
+  <ul
+  className="positive feedback" >Positive feedback:
+  </ul>
+</li>
     </div>
   );
 };
