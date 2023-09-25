d98e2d9e4d83f63f9594ce0e5c177dfcde0a8a9b
Igor2109 <127054357+Igor2109@users.noreply.github.com>
2023-09-25 22:10:21 +0200

    state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,37 @@
-import css from "./App.module.css"
+import React, { Component } from 'react';
+
+import css from './App.module.css';
 import Statistics from "./Statistics/Statistics";
-export const App = () => {
-  return (
-    <div>
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  changeState = () => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <button onClick={this.changeState} type="button" className={css.btn}>
+          Good
+        </button>
+        <button type="button" className={css.btn}>
+          Neutral
+        </button>
+        <button type="button" className={css.btn}>
+          Bad
+        </button>
 
-
-<Statistics/>
-    </div>
-
-  );
-};
+        <h2>Statistics</h2>
+        <Statistics good={this.state.good} bad={this.state.bad} neutral={this.state.neutral}/>
+      </div>
+    );
+  }
+}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,66 +1,17 @@
-import React, { Component } from 'react'
-import css from "../App.module.css"
+import React, { Component } from 'react';
+import css from '../App.module.css';
 
 export default class Statistics extends Component {
 
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-      }
-
   render() {
     return (
-      <div>
-      <h2>Please leave feedback</h2>
-      <button type="button" className={css.btn}>
-        Good
-      </button>
-      <button type="button" className={css.btn}>
-        Neutral
-      </button>
-      <button type="button" className={css.btn}>
-        Bad
-      </button>
-
-      <h2>Statistics</h2>
-<li>
-  <ul
-  value={this.state.good}
-  name="good" >Good: {this.state.good}
-  </ul>
-  <ul
-  className="neutral" >Neutral: {this.state.neutral}
-  </ul>
-  <ul
-  className="bad" >Bad: {this.state.bad}
-  </ul>
-  <ul
-  className="total" >Total:
-  </ul>
-  <ul
-  className="positive feedback" >Positive feedback:
-  </ul>
-</li>
-<form action="">
-<label>
-    <span>Good:</span>
-    <input value={this.state.good}
-  name="good" type="text" />
-</label>
-<label>
-    <span>Neutral:</span>
-    <input value={this.state.neutral}
-  name="neutral" type="text" />
-</label>
-<label>
-    <span>Bad:</span>
-    <input value={this.state.bad}
-  name="bad" type="text" />
-</label>
-</form>
-
-      </div>
-    )
+      <ul>
+        <li>{`Good: ${this.props.good}`}</li>
+        <li className="neutral">Neutral: {this.props.neutral}</li>
+        <li className="bad">Bad: {this.props.bad}</li>
+        <li className="total">Total:</li>
+        <li className="positive feedback">Positive feedback:</li>
+      </ul>
+    );
   }
 }
