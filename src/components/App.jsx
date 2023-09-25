8b31f6986af8ddd85bc0d22370b1fbf293cf664e
Igor2109 <127054357+Igor2109@users.noreply.github.com>
2023-09-25 19:45:46 +0200

    feadback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,12 @@
 import css from "./App.module.css"
+import Statistics from "./Statistics/Statistics";
 export const App = () => {
   return (
     <div>
-      <h2>Please leave feedback</h2>
-      <button type="button" className={css.btn}>
-        Good
-      </button>
-      <button type="button" className={css.btn}>
-        Neutral
-      </button>
-      <button type="button" className={css.btn}>
-        Bad
-      </button>
 
-      <h2>Statistics</h2>
-<li>
-  <ul
-  className="good" >Good:
-  </ul>
-  <ul
-  className="neutral" >Neutral:
-  </ul>
-  <ul
-  className="bad" >Bad:
-  </ul>
-  <ul
-  className="total" >Total:
-  </ul>
-  <ul
-  className="positive feedback" >Positive feedback:
-  </ul>
-</li>
+
+<Statistics/>
     </div>
+
   );
 };
